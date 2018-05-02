be535823c7ab2fb30e364dc05f57bbdcb3ddc546
the Tim Ridenour <theTimRidenour@gmail.com>
2018-05-02 15:56:47 -0500

    first commit

---
+++ src/test/java/com/entertainment/RecSub/RecSubApplicationTests.java
@@ -0,0 +1,16 @@
+package com.entertainment.RecSub;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RecSubApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
