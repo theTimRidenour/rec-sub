aacd0dd5baaa442780ab5ab2d480a48dc055b27f
the Tim Ridenour <theTimRidenour@gmail.com>
2018-05-02 18:09:22 -0500

    changed from java 10 to java 8

--- src/main/java/com/entertainment/RecSub/RecSubApplication.java
+++ src/main/java/com/entertainment/RecSub/RecSubApplication.java
@@ -1,4 +1,4 @@
-package com.entertainment.RecSub;
+package com.entertainment.recsub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/entertainment/RecSub/RecSubApplicationTests.java
+++ src/test/java/com/entertainment/RecSub/RecSubApplicationTests.java
@@ -1,4 +1,4 @@
-package com.entertainment.RecSub;
+package com.entertainment.recsub;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
